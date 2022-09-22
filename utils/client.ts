f6ab8bcb258e2535f7d4c80c454e4b8c7b5bab7f
Duc Thai <ducthai060501@gmail.com>
2022-09-22 05:06:06 +0300

    video upload functionality

--- utils/client.ts
+++ utils/client.ts
@@ -5,5 +5,5 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
-  token: process.env.NEXT_TOKEN,
+  token: 'sksGCK5SrfzGdxIIKRcvgA31E6hFSBOBne6Vk6meMsauLk5nxd80MCHaKmzbkDTwtveg3kcYp9e9bZrOnPMyyN3JP8UobsbSd1Yw4LaCXNtWrsBxfkFaCnR0FB6U3M6mV6pH8VkR6gMysifDi9LkR3K4PMM3TUW0RTgz6MMV0IzZUsW6f2gX',
 });
