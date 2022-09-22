810a3ee8993c1627aa017e8381ca55c28679014b
Duc Thai <ducthai060501@gmail.com>
2022-09-22 15:50:45 +0300

    render comments functionality

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -22,14 +22,56 @@ interface IProps {
   comments: IComment[];
 }
 
-const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
- 
-  const { userProfile }: any = useAuthStore();
+const Comments = ({
+  comment,
+  setComment,
+  addComment,
+  comments,
+  isPostingComment,
+}: IProps) => {
+  const { userProfile, allUsers }: any = useAuthStore();
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0  pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
         {comments?.length ? (
-          <div>videos</div>
+          comments.map((item, index) => (
+            <>
+              {allUsers.map(
+                (user: IUser) =>
+                  user._id === (item.postedBy._id || item.postedBy._ref) && (
+                    <div className="p-2 items-center" key={index}>
+                      <Link href={`/profile/${user._id}`}>
+                        <div className="flex items-start gap-3">
+                          <div className="w-8 h-8">
+                            <Image
+                              src={user.image}
+                              width={34}
+                              height={34}
+                              className="rounded-full"
+                              alt="user profile"
+                              layout="responsive"
+                            />
+                          </div>
+                          <div className="xl:block">
+                            <p className="flex gap-1 text-md font-bold text-primary lowercase">
+                              {user.userName.replaceAll(" ", "")}
+                              <GoVerified className="text-blue-400" />
+                            </p>
+                            <p className="capitalize text-gray-400 text-sm">
+                              {user.userName}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                      <div>
+                        <p>{item.comment}</p>
+
+                      </div>
+                    </div>
+                  )
+              )}
+            </>
+          ))
         ) : (
           <NoResult text="No comments yet, be the first one to comment" />
         )}
@@ -44,11 +86,8 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
               className="bg-primary px-6 py-4 text-md font-md border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 
               focus:outline-none focus:border-gray-300 flex-1 rounded-lg"
             />
-            <button 
-            
-                onClick = {addComment}
-              className="text-md text-gray-400">
-              {isPostingComment ? 'Commernting...' : 'Comment'}
+            <button onClick={addComment} className="text-md text-gray-400">
+              {isPostingComment ? "Commernting..." : "Comment"}
             </button>
           </form>
         </div>
--- store/authStore.ts
+++ store/authStore.ts
@@ -1,13 +1,23 @@
 import React from "react";
 import create from "zustand";
 import {persist} from "zustand/middleware" 
+import { BASE_URL } from "../utils";
 
 import axios from 'axios' 
 
+
+
 const authStore = (set: any) => ({
     userProfile: null  , 
+    allUsers: [] ,
     addUser: (user: any) => set({userProfile: user}),
-    removeUser: () => set({userProfile: null} )
+    removeUser: () => set({userProfile: null} ),
+
+    fetchAllUsers : async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`)
+
+        set({allUsers: response.data})
+    }
 })
 
 const useAuthStore = create(
